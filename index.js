6d4a4c40c5daf5819381caa190afd8071275f943
Sandeep Sunny Nair <sunnynair54@yahoo.com>
2023-01-19 21:33:49 -0600

    step 2 completed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const PORT = 3000;
 const express = require('express');
 const server = express();
 const apiRouter = require('./api');
-server.use('/api', apiRouter);
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
@@ -14,13 +14,15 @@ server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
     console.log(req.body);
     console.log("<_____Body Logger END_____>");
-
+    
     next();
 });
 
 
 const { client } = require('./db');
 client.connect();
+
+server.use('/api', apiRouter);
 
 server.listen(PORT, () => {
     console.log('The server is up on port', PORT)
