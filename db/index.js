f509b5e14f5516ea2fc6b830a6d028b09107d230
Sandeep Sunny Nair <sunnynair54@yahoo.com>
2023-01-22 18:43:38 -0600

    patched the URL

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { Client } = require('pg') // imports the pg module
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
+const { DATABASE_URL } = process.env;
+const URL = require('dotenv').config();
 
 /**
  * USER Methods
