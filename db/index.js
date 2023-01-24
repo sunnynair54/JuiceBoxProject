8d7575f9d82b8a82803b2f564da89cea79294495
Sandeep Sunny Nair <sunnynair54@yahoo.com>
2023-01-23 21:59:14 -0600

    Log added

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ const { Client } = require('pg') // imports the pg module
 
 require('dotenv').config();
 const { DATABASE_URL } = process.env;
+console.log(DATABASE_URL)
 const client = new Client(DATABASE_URL);
 /**
  * USER Methods
